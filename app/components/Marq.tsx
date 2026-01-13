0d073bd22c79ccf8e2c619efa0c575cc394adb13
agorapilled <johngardner13@outlook.com>
2026-01-13 00:15:31 -0500

    v0

--- app/components/Marq.tsx
+++ app/components/Marq.tsx
@@ -46,7 +46,7 @@ export function Marqueee({
     <div
       {...props}
       className={cn(
-        "group flex [gap:var(--gap)] overflow-hidden p-2 [--duration:40s] [--gap:1rem]",
+        "group flex [gap:var(--gap)] overflow-hidden p-1 [--duration:40s] [--gap:1rem]",
         {
           "flex-row": !vertical,
           "flex-col": vertical,
